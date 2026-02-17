71bfed8f8c4688325de8174d9e152d371616deb7
danilofloe77 <danilonavarro003@hotmail.com>
2026-02-17 11:28:41 -0500

    Vaciar

--- Holidayproject-danilo/supervisor.js
+++
@@ -1,6 +0,0 @@
-function mostrarSeccion(id) {
-    document.querySelector('.contenido-principal').style.display = 'block';
-    const secciones = document.querySelectorAll('.seccion');
-    secciones.forEach(sec => sec.classList.remove('activa'));
-    document.getElementById(id).classList.add('activa');
-}
